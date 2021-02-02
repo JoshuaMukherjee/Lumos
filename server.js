c38d8ae5c3769b0c3480188366e560ff327294d6
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-02-02 16:46:30 +0000

    formatting

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ http.createServer(function (req, res) {
 
         var params = url.parse(req.url,true).query;
         if(params.state == 1){
-            console.log(1);
+            console.log(1); //TODO turn light on
         }else if(params.state == 0){
-            console.log(0);
+            console.log(0); //TODO turn light off
         }
-
         return res.end();
       });
 }).listen(8080); //the server object listens on port 8080
