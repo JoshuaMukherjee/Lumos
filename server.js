ee3ce48e1554549072799b2fe8c44852eca8b6d1
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2021-02-04 11:56:28 +0000

    loop

--- server.js
+++ server.js
@@ -5,24 +5,25 @@ const Gpio = require('pigpio').Gpio;
 
 const motor = new Gpio(14, {mode: Gpio.OUTPUT}); //create servo
 
-//create a server object:
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data.toString());
-	    console.log("Starting");
-        var params = url.parse(req.url,true).query;
-        if(params.state == 1){
-            left()
-        }else if(params.state == 0){
-            right()
-        }
-	    setTimeout(() => {  resetMotor() }, 1000);
+while(true){
+    //create a server object:
+    http.createServer(function (req, res) {
+        fs.readFile('index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data.toString());
+            console.log("Starting");
+            var params = url.parse(req.url,true).query;
+            if(params.state == 1){
+                left()
+            }else if(params.state == 0){
+                right()
+            }
+            setTimeout(() => {  resetMotor() }, 1000);
 
-        return res.end();
-      });
-}).listen(8080); //the server object listens on port 8080
-
+            return res.end();
+        });
+    }).listen(8080); //the server object listens on port 8080
+}
 
 var left = function(){
     //ON
